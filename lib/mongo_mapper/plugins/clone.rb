85c0ea5477874ea8cfd792e3c836182fae9b0b71
Nurahmadie <nurahmadie@gmail.com>
2020-02-01 15:59:50 +0700

    Fix clone and document keys spec

--- lib/mongo_mapper/plugins/clone.rb
+++ lib/mongo_mapper/plugins/clone.rb
@@ -7,12 +7,13 @@ module MongoMapper
       def initialize_copy(other)
         @_new       = true
         @_destroyed = false
-        remove_instance_variable :@_id if instance_variable_defined?(:@_id)
+
+        @attributes = self.class._default_attributes.deep_dup
 
         associations.each do |name, association|
           instance_variable_set(association.ivar, nil)
         end
-        self.attributes = other.attributes.clone.except(:_id).inject({}) do |hash, entry|
+        self.attributes = other.attributes.clone.except('_id').inject({}) do |hash, entry|
           key, value = entry
           hash[key] = value.duplicable? ? value.clone : value
           hash
